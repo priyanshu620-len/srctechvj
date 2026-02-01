87f2c78762af7c1b171d947f607c0cd220558f8d
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-01 22:47:52 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -62,10 +62,10 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/kingvj01")
+        InlineKeyboardButton("❣️ Developer", url = "https://t.me/Dexter_Onex")
     ],[
         InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vj_bot_disscussion'),
-        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/vj_bots')
+        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/+Nvjvwd4cxkM5YWI1')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
